e12d40f228ec1f4bf157fa79f7743df788d0c3d5
GabnielDev <gabnieldev@gmail.com>
2021-03-19 20:33:35 -0700

    First init

--- app/src/main/java/com/example/bandungvaporizer/MainActivity.kt
+++ app/src/main/java/com/example/bandungvaporizer/MainActivity.kt
@@ -2,11 +2,70 @@ package com.example.bandungvaporizer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.example.bandungvaporizer.fragment.AkunFragment
+import com.example.bandungvaporizer.fragment.HomeFragment
+import com.example.bandungvaporizer.fragment.KeranjangFragment
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    val fragmentHome : Fragment = HomeFragment()
+    val fragmentKeranjang : Fragment = KeranjangFragment()
+    val fragmentAkun : Fragment = AkunFragment()
+    val fm : FragmentManager = supportFragmentManager
+    var active : Fragment = fragmentHome
+
+    private lateinit var menu : Menu
+    private lateinit var menuItem: MenuItem
+    private lateinit var bottomNavigationView: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setUpBotNav()
+
     }
+
+    fun setUpBotNav() {
+        fm.beginTransaction().add(R.id.container, fragmentHome).show(fragmentHome).commit()
+        fm.beginTransaction().add(R.id.container, fragmentKeranjang).hide(fragmentKeranjang).commit()
+        fm.beginTransaction().add(R.id.container, fragmentAkun).hide(fragmentAkun).commit()
+
+        bottomNavigationView = findViewById(R.id.nav_view)
+        menu = bottomNavigationView.menu
+        menuItem = menu.getItem(0)
+        menuItem.isChecked = true
+
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+
+            when(item.itemId) {
+                R.id.navigation_home -> {
+                    callFragment(0, fragmentHome)
+                }
+                R.id.navigation_keranjang -> {
+                    callFragment(1, fragmentKeranjang)
+                }
+                R.id.navigation_akun -> {
+                    callFragment(2, fragmentAkun)
+                }
+            }
+
+            false
+        }
+    }
+
+    fun callFragment(int: Int, fragment: Fragment) {
+        menuItem = menu.getItem(int)
+        menuItem.isChecked = true
+        fm.beginTransaction().hide(active).show(fragment).commit()
+        active = fragment
+    }
+
 }
